5cfbacaf50c363cd4e3dafabd6037e70dec061f2
Avani Mandlik <avanimandlik27@gmail.com>
2025-09-13 12:58:59 +0530

    Update 3: added 2 more pages

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+//win_vs_fed js
+//to give out yes buttons in random-like ugly way
 const nobtn = document.getElementById("nobtn");
 const yesbutton6 = document.querySelectorAll(".col-6.d-none");
-const yesbutton4 = document.querySelectorAll(".col-4.d-none");
+const yesbutton1 = document.querySelectorAll(".col-1.d-none");
 const yesbutton12 = document.querySelectorAll(".col-12.d-none");
 const yesbutton3 = document.querySelectorAll(".col-3.d-none");
-const foot = document.querySelector(".foot.d-none");
+const foot = document.querySelectorAll(".foot.d-none");
 let yescount6=0;
 let yescount12=0;
 let yescount3=0;
-let yescount4=0;
+let yescount1=0;
 console.log(yesbutton6.length);
 
 nobtn.addEventListener("click", ()=>{
-    console.log("Hello, no is working");
+    // console.log("Hello, no is working");
     if(yesbutton6.length>yescount6){
         yesbutton6[yescount6].classList.remove("d-none");
         yescount6++;
@@ -24,11 +26,19 @@ nobtn.addEventListener("click", ()=>{
         yesbutton3[yescount3].classList.remove("d-none");
         yescount3++;
     }
-    if(yesbutton4.length>yescount4){
-        yesbutton4[yescount4].classList.remove("d-none");
-        yescount4++;
+    if(yesbutton1.length>yescount1){
+        yesbutton1[yescount1].classList.remove("d-none");
+        yescount1++;
     }
-    if(yescount4 === 2 && yescount3 === 2 && yescount12 === 2 && yescount6 === 2){
+    if(yescount1 === 3 && yescount3 === 3 && yescount12 === 3 && yescount6 === 3){
+        console.log("We done");
         foot.classList.remove("d-none");
     }
+})
+
+//this will take you back to wf html
+const backtowf = document.getElementById("backtowf");
+
+backtowf.addEventListener("click", ()=>{
+    
 })
