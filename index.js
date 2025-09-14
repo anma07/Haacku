795c1f632f6c4f51a83bc8aa2f878e371faf0d7c
Avani Mandlik <avanimandlik27@gmail.com>
2025-09-14 16:03:54 +0530

    updated wf and ttt

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+//most of the console logs are added to test stuff, and i dont want to take them out, cuz so much green green in between the code looks so cool in vs code
+
 //win_vs_fed js
 //to give out yes buttons in random-like ugly way
+
 const nobtn = document.getElementById("nobtn");
 const yesbutton6 = document.querySelectorAll(".col-6.d-none");
 const yesbutton1 = document.querySelectorAll(".col-1.d-none");
 const yesbutton12 = document.querySelectorAll(".col-12.d-none");
 const yesbutton3 = document.querySelectorAll(".col-3.d-none");
-const foot = document.querySelectorAll(".foot.d-none");
+const foot = document.querySelector(".foot.d-none");
 let yescount6=0;
 let yescount12=0;
 let yescount3=0;
 let yescount1=0;
-let flag=0;
 // console.log(yesbutton6.length);
 
-nobtn.addEventListener("click", ()=>{
+if(nobtn){
+    nobtn.addEventListener("click", ()=>{
     // console.log("Hello, no is working");
     if(yesbutton6.length>yescount6){
         yesbutton6[yescount6].classList.remove("d-none");
@@ -32,32 +35,46 @@ nobtn.addEventListener("click", ()=>{
         yescount1++;
     }
     if(yescount1 === 3 && yescount3 === 3 && yescount12 === 3 && yescount6 === 3){
-        console.log("We done");
+        // console.log("We done");
         foot.classList.remove("d-none");
     }
 })
+}
 
 //this will take you back to wf html with special message
 const bait1 = document.getElementById("bait1");
-const rb1 = document.querySelectorAll(".col-11.d-none");
+const rb1 = document.querySelector(".col-11.d-none");
 console.log(rb1);
 
-bait1.addEventListener("click", ()=>{
+if(bait1){
+    bait1.addEventListener("click", ()=>{
     localStorage.setItem("flag", "1");
     window.location.href="win_vs_fed.html";
 })
+}
 
-console.log(rb1.length);
-
-if (localStorage.get("flag") === "1") {
-    rb1[0].classList.remove("d-none");
+if(rb1){
+    console.log(rb1.length);
+    if (localStorage.getItem("flag") === "1") {
+    rb1.classList.remove("d-none");
+}
 }
 
 //start the legendary TIKTAKTOS
-const tttstart = document.getElementById("tttstart");
-const body = document.querySelectorAll(".bodyttt");
 
-tttstart.addEventListener("click", ()=>{
-    console.log("tttstart was pressed");
-    body.classList.add("black");
+const tttstart = document.getElementById("tttstart");
+const bodyttt = document.querySelector(".bodyttt");
+const tttback = document.querySelector(".tttlight.d-none");
+
+// console.log(bodyttt.length);
+
+if(tttstart){
+    tttstart.addEventListener("click", ()=>{
+    // console.log("tttstart was pressed");
+    bodyttt.classList.add("black");
+    tttback.classList.remove("d-none");
 })
+}
+//TIKTAKTOS code
+const square = document.querySelectorAll(".square");
+// console.log(cell.length);
